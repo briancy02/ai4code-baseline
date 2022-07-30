fix: computation of kendaul tau for alt dataset

code/model.py
@@ -9,17 +9,17 @@ from pathlib import Path
 from pretrain_longformer import PretrainingModel
 
 class MarkdownModel(nn.Module):
-    def __init__(self, model_path, md_max_len, using_pretrained, num_gpus):
+    def __init__(self, model_path, md_max_len, num_gpus, pretrained_model_path=None):
         super(MarkdownModel, self).__init__()
         self.attention_window = 512
         self.md_max_len = md_max_len
         self.max_input_len = 512
         self.model = AutoModel.from_pretrained(model_path)
-        if using_pretrained:
+        if pretrained_model_path:
             model = RobertaForMaskedLM.from_pretrained("microsoft/codebert-base-mlm")
             model = nn.DataParallel(model, device_ids=[i for i in range(num_gpus)])
             model.to("cuda")
-            checkpoint = torch.load(str(Path.cwd())+"/outputs/model-0.bin")
+            checkpoint = torch.load(str(Path.cwd())+pretrained_model_path)
             #print(checkpoint.keys())
             model.load_state_dict(checkpoint)
             self.model = model.module.roberta
@@ -32,7 +32,7 @@ class MarkdownModel(nn.Module):
         return x
 
 class LongformerModel(nn.Module):
-    def __init__(self, model_path, md_max_len, using_pretrained, num_gpus):
+    def __init__(self, model_path, md_max_len, num_gpus, using_pretrained):
         super(LongformerModel, self).__init__()
         self.attention_window = 512
         self.md_max_len = md_max_len

code/train.py
@@ -14,9 +14,10 @@ from metrics import *
 import torch
 import argparse
 from stlr import SlantedTriangular
-parser.add_argument('--data_dir', type=str, default="processed_dataset_1/")
 
 parser = argparse.ArgumentParser(description='Process some arguments')
+parser.add_argument('--data_dir', type=str, default="processed_dataset_2/")
+
 parser.add_argument('--model_name_or_path', type=str, default='microsoft/codebert-base')
 parser.add_argument('--train_mark_path', type=str, default='train_mark.csv')
 parser.add_argument('--train_features_path', type=str, default='train_fts.json')
@@ -24,8 +25,8 @@ parser.add_argument('--val_mark_path', type=str, default='val_mark.csv')
 parser.add_argument('--val_features_path', type=str, default='val_fts.json')
 parser.add_argument('--val_path', type=str, default='val.csv')
 parser.add_argument('--orders_data_path', type=str, default='train_orders.csv')
-parser.add_argument('--checkpoint_format', type=str, default="./outputs/codebert_new_data/model-{e}.bin")
-
+parser.add_argument('--checkpoint_format', type=str, default="./outputs/codebert_second_data/model-{e}.bin")
+parser.add_argument('--pretrained_model_path', type=str, default="/outputs/model-0.bin")
 
 parser.add_argument('--num_gpus', type=int, default=4)
 parser.add_argument('--md_max_len', type=int, default=64)
@@ -39,12 +40,10 @@ parser.add_argument('--n_workers', type=int, default=8)
 args = parser.parse_args()
 data_dir = str(Path.cwd()) + '/data/' + args.data_dir
 
-#os.mkdir("./outputs")
-
 print("MODEL CONFIGS")
 
 if args.data_dir == "processed_dataset_2/":
-    train_df_mark = pd.read_csv(data_dir+args.train_mark_path +).dropna().reset_index(drop=True)
+    train_df_mark = pd.read_csv(data_dir+args.train_mark_path).dropna().reset_index(drop=True)
     val_df_mark = pd.read_csv(data_dir+args.val_mark_path).dropna().reset_index(drop=True)
     df_orders = pd.read_csv(
         data_dir + 'data.csv',
@@ -60,8 +59,8 @@ else:
         index_col='id',
         squeeze=True,
     ).str.split() 
-train_fts = json.load(data_dir+open(args.train_features_path))
-val_fts = json.load(data_dir+open(args.val_features_path))
+train_fts = json.load(open(data_dir+args.train_features_path))
+val_fts = json.load(open(data_dir+args.val_features_path))
 val_df = pd.read_csv(data_dir+args.val_path)
 
 train_ds = MarkdownDataset(train_df_mark, model_name_or_path=args.model_name_or_path, md_max_len=args.md_max_len,
@@ -80,7 +79,7 @@ def collate_fn_padd(batch):
     
     return inputs, masks, fts, ranks
 
-val_ds = MarkdownDataset(val_df_mark, training_corpus, model_name_or_path=args.model_name_or_path, md_max_len=args.md_max_len,
+val_ds = MarkdownDataset(val_df_mark, model_name_or_path=args.model_name_or_path, md_max_len=args.md_max_len,
                          total_max_len=args.total_max_len, fts=val_fts)
 train_loader = DataLoader(train_ds, batch_size=args.batch_size, shuffle=True, num_workers=args.n_workers,
                           pin_memory=False, drop_last=True, collate_fn = collate_fn_padd)
@@ -139,14 +138,14 @@ def train(model, train_loader, val_loader, epochs):
     
     resume_from_epoch = 0
     for try_epoch in range(epochs, 0, -1):
-        if os.path.exists('./outputs/codebert_new_data/model-{epoch}.bin'.format(epoch=try_epoch)):
+        if os.path.exists('./outputs/codebert_second_data/model-{epoch}.bin'.format(epoch=try_epoch)):
             resume_from_epoch = try_epoch+1
             break
     if resume_from_epoch:
         filepath = args.checkpoint_format.format(e=resume_from_epoch)
         checkpoint = torch.load(args.checkpoint_format.format(e=try_epoch))
         model.load_state_dict(checkpoint)
-        
+    y_pred = None    
     for e in range(resume_from_epoch, epochs):
         model.train()
         tbar = tqdm(train_loader, file=sys.stdout)
@@ -178,18 +177,23 @@ def train(model, train_loader, val_loader, epochs):
         # objective is to learn the percentage ranking
         y_val, y_pred = validate(model, val_loader)
         # display rankings in percentage
+        torch.save(model.state_dict(), args.checkpoint_format.format(e=e))
         val_df["pred"] = val_df.groupby(["id", "cell_type"])["rank"].rank(pct=True)
         val_df.loc[val_df["cell_type"] == "markdown", "pred"] = y_pred
-        y_dummy = val_df.sort_values("pred").groupby('id')['cell_id'].apply(list)
+        #y_dummy = val_df.sort_values("pred").groupby('id')['cell_id'].apply(list)
+        y_dummy = val_df.sort_values("pred").groupby('id')['Unnamed: 0'].apply(list)
+        new_df = df_orders.groupby(['id']).agg(tuple).applymap(list).reset_index().set_index('id')
+        print("df_orders", df_orders.loc[y_dummy.index])
+        print(y_dummy)
+        print("Preds score", kendall_tau(new_df.loc[y_dummy.index]['Unnamed: 0'], y_dummy))
+        
         #print("Preds score", kendall_tau(df_orders.loc[y_dummy.index], y_dummy))
-        torch.save(model.state_dict(), args.checkpoint_format.format(e=e))
-        print("Preds score", kendall_tau(df_orders[df_orders.id==y_dummy.index], y_dummy))
         
 
     return model, y_pred
 
 
-model = MarkdownModel(args.model_name_or_path, args.md_max_len, using_pretrained=True, num_gpus=args.num_gpus)
+model = MarkdownModel(args.model_name_or_path, args.md_max_len, num_gpus=args.num_gpus, pretrained_model_path=args.pretrained_model_path)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 if torch.cuda.device_count() > 1: